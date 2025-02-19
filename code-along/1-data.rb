6c4cb2897443d4f0dde6dbfa198774aef68a151e
Jahan Ara Saleem <jahan.saleem@kellogg.northwestern.edu>
2025-02-19 03:49:45 +0000

    Lab1done

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -6,14 +6,47 @@
 
 # Numbers
 
+puts 1
+puts 2
+puts 6+9
+puts 1/4.0
+
 # Non-numbers
+
+puts "Hello!!"
+
+puts "Hellowww" + "WOAH"
+
+puts "tacos" *4
+
+puts "tacos" + 3.to_s # lasdjf
 
 # True, False
 
+puts true
+
+puts false
+
 # Nothing
+
+puts nil
 
 # Variables
 
+alpha = "tacos"
+
+count = 3
+
+puts alpha*count
+
 # Combine strings and variables
 
+puts "tacos: #{count}"
+
+puts "#{alpha}: #{count}"
+
 # String manipulation
+
+puts "pspspss".length
+puts "oh no".capitalize
+puts "ohio".reverse
